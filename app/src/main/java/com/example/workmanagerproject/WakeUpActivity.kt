b21ee5e9223ebea5cedc61d139545abe755ee99b
mariam-elkirch <mariamk019@gmail.com>
2022-09-17 23:22:59 +0200

    remove comments

--- app/src/main/java/com/example/workmanagerproject/WakeUpActivity.kt
+++ app/src/main/java/com/example/workmanagerproject/WakeUpActivity.kt
@@ -3,40 +3,37 @@ package com.example.workmanagerproject
 import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
-import android.content.BroadcastReceiver
-import android.content.Intent
-import android.content.IntentFilter
+import android.annotation.SuppressLint
+import android.app.KeyguardManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
 import android.view.WindowManager
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.MotionEventCompat
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 
 class WakeUpActivity : AppCompatActivity() {
     lateinit var objectAnimator : ObjectAnimator
     private lateinit var textScreen : View
     val DEFAULT_ANIMATION_DURATION = 2500L
+    @RequiresApi(Build.VERSION_CODES.O_MR1)
+    @SuppressLint("ServiceCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wake_up)
         textScreen = findViewById(R.id.screen)
-        window.addFlags(
-            WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON or
-                    WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
-                    WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
-                    WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+        allowOnLockScreen()
+        animationAppearance()
 
-        )
+    }
 
-
+    private fun animationAppearance() {
         objectAnimator = ObjectAnimator.ofObject(
             textScreen,
             "backgroundColor",
@@ -45,16 +42,13 @@ class WakeUpActivity : AppCompatActivity() {
             ContextCompat.getColor(this, R.color.black)
         )
 
-// 2
         objectAnimator.repeatCount = 1000
         objectAnimator.repeatMode = ValueAnimator.REVERSE
 
-// 3
         objectAnimator.duration = DEFAULT_ANIMATION_DURATION
         objectAnimator.start()
-
-
     }
+
     override fun onTouchEvent(event: MotionEvent): Boolean {
         return when (MotionEventCompat.getActionMasked(event)) {
 
@@ -64,6 +58,18 @@ class WakeUpActivity : AppCompatActivity() {
                 true
             }
             else -> super.onTouchEvent(event)
+        }
+    }
+    private fun allowOnLockScreen() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setShowWhenLocked(true)
+            setTurnScreenOn(true)
+            val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+            keyguardManager.requestDismissKeyguard(this, null)
+        } else {
+            this.window.addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
+                    WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
+                    WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON)
         }
     }
 }
